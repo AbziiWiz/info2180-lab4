634edcdf28edf3875eb3fb267eb5cab0cb423f1c
AbziiWiz <abigailthomas279@gmail.com>
2016-10-11 19:45:50 -0400

    Add files via upload
    Adding files to branch

---
+++ maze.js
@@ -0,0 +1,53 @@
+//Javascript for lab 4
+
+//Setting up window onload
+window.onload = myFunction(){
+	
+	//this should have all the eventlisenters
+	function myFunction()
+	{
+		var name = "";
+		
+		if (name === "S")
+		{
+			
+		} else if(){
+			
+		}
+		else if (){
+			
+		}	
+		else if (){
+			
+		}	
+		else if (){
+			
+		}
+		else if(){
+			
+		}
+	}
+	
+}
+
+//Once user hovers over S text change game begins maybe addtional
+
+
+
+//Once Outside of maze color change for walls
+
+//Change for first boundary
+
+//Change for second boundary
+
+//Change for third boundary
+
+//Change for fourth boundary
+
+//Change for fifth boundary
+
+//If outside maze text change to you lose
+
+//Once user hovers over E text change "You Win"
+
+
