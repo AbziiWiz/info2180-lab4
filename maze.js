bf53bfabbf4e0980fa76f3e5094c42c8e960a853
AbziiWiz <abigailthomas279@gmail.com>
2016-10-14 18:01:31 -0400

    Update maze.js
    finish ex.5 and changes to ex.4

--- maze.js
+++ maze.js
@@ -25,14 +25,36 @@ window.onload = function (){
 	});
 	
 	
+	//Ex.2
+	var walls = document.querySelectorAll(".boundary");
+	var winner = true;
+	var begin;
+	var ele = document.getElementById("status");
+	for (var i=0; i<walls.length; i++){
+		walls[i].addEventListener("mouseover", function()
+		{
+			for (var j=0; j< walls.length;j++){
+				walls[j].setAttribute("class", "boundary youlose");
+				
+			}
+			begin = false;
+			winner = false;
+			ele.textContent = "You lose :^(";
+			
+		});
+	}
+	
+	
 	//Ex.3
 	var end = document.getElementById("end");
 	end.addEventListener("mouseover", function(){
+		//Can also be if(!winner)
 		if (winner){
-			alert("You Win!");
+			
+				ele.textContent = "You Win!";
 		}
-	});	
-	
+	});
+
 	
 	//Ex.4
 	var start = document.getElementById("start");
@@ -44,8 +66,11 @@ window.onload = function (){
 			walls[x].classList.remove("youlose");
 			//walls[x].setAttribute("class","boundary");
 		}
+		
+		ele.textContent ='Mouse your mouse over the "S" to begin.';
 		winner = true;
 	};
-	
+
+
 	
 }
